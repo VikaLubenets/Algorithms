3ae7afba1647bc16437b3b30dd294e6aace3a148
Vika Lubenets <lubenetcvika@gmail.com>
2024-04-17 19:30:35 +0400

    feat: add new task

--- Algorithms in JS/Stack/task1.js
+++ Algorithms in JS/Stack/task1.js
@@ -35,25 +35,52 @@ console.log(checkBrackets('((())')) // false
 
 // ## Реализовать стек на основе типизированного массива заданной длины
 
-// class Stack {
+class Stack {
+  constructor(typeArr, size){
+    this.stack = new typeArr(size);
+    this.size = size;
+    this.head = null;
+    this.length = 0;
+  }
 
-// }
+  push(el){
+    if(this.length >= this.size){
+      return new Error('Stack overflow: the stack is full')
+    }
+    this.head = el;
+    this.stack[this.length] = el;
+    this.length++;
+  }
 
+  pop(){
+    if (this.length <= 0) {
+      throw new Error('The stack is empty');
+    }
+    const el = this.stack[this.length - 1];
+    this.stack[this.length - 1] = null;
+    this.length--;
+    if(this.length === 0){
+      this.head = null;
+    } else {
+      this.head = this.stack[this.length - 1];
+    }
+    return el;
+  }
+}
 
-
-  //  const stack = Stack(Int32Array, 10);
+   const stack = new Stack(Int32Array, 10);
    
-  //  stack.push(10);
-  //  stack.push(11);
-  //  stack.push(12);
+   stack.push(10);
+   stack.push(11);
+   stack.push(12);
    
-  //  console.log(stack.head);  // 12
+   console.log(stack.head);  // 12
    
-  //  console.log(stack.pop()); // 12
+   console.log(stack.pop()); // 12
    
-  //  console.log(stack.head);  // 11
+   console.log(stack.head);  // 11
    
-  //  console.log(stack.pop()); // 11
-  //  console.log(stack.pop()); // 10
-  //  console.log(stack.pop()); // Exception
+   console.log(stack.pop()); // 11
+   console.log(stack.pop()); // 10
+   console.log(stack.pop()); // Exception
 //    ```
